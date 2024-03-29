8b8bed9677941265c7d1a27c812ed8bb5a43bde6
GenRubio <burbianio@gmail.com>
2024-03-29 10:17:33 +0100

    WIP :: First Commit

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+import * as ScreenOrientation from 'expo-screen-orientation';
+import { Camera } from 'expo-camera';
+import { Audio } from 'expo-av';
 
 export default function App() {
+
+  /**
+   * Change screen orientation to LANDSCAPE_LEFT
+   */
+  changeScreenOrientation();
+  async function changeScreenOrientation() {
+    await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE_LEFT);
+  }
+
+  /**
+   * Request camera and audio permissions
+   */
+  const [hasPermission, setHasPermission] = React.useState(null);
+  React.useEffect(() => {
+    (async () => {
+      const { status: cameraStatus } = await Camera.requestCameraPermissionsAsync();
+      const { status: audioStatus } = await Audio.requestPermissionsAsync();
+      setHasPermission(cameraStatus === 'granted' && audioStatus === 'granted');
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <WebView source={{ uri: 'https://suara.pre.basetis.com' }} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: '100%',
+    height: '100%',
   },
+
 });
